cd9103aa6960bea7a4e6db90b018cab61e4d264d
Shachar1120 <148318799+Shachar1120@users.noreply.github.com>
2024-03-22 14:44:13 +0200

    Update tkinter_try.py

--- mytkinter_examples/tkinter_try.py
+++ mytkinter_examples/tkinter_try.py
@@ -9,7 +9,7 @@ class A:
         # sets the geometry of main
         # root window
         self.root.geometry("300x300")
-        self.root.title("Register or sign in")
+        self.root.title("Register or Log in")
         self.label = Label(self.root,
                       text="Welcome to VidPal!")
 
@@ -21,7 +21,7 @@ class A:
 
 
         # Create a Button
-        self.btn_assign = Button(self.root, text='Sign In', command=self.Assign_Window)
+        self.btn_assign = Button(self.root, text='Log In', command=self.Assign_Window)
         #self.btn_reg.pack(side=RIGHT)
         self.btn_assign.place(x=200, y=100)
 
@@ -40,28 +40,54 @@ class A:
         # sets the geometry of toplevel
         newWindow.geometry("450x300")
 
-        # A Label widget to show in toplevel
-        Label(newWindow,
-              text="This is a new window").pack()
-
-
         # the label for user_name
         self.user_name = Label(newWindow,text="Username")
         self.user_name.place(x=40,y=60)
 
         # the label for user_password
-        self.user_password = Label(newWindow,text="Password").place(x=40,y=100)
+        self.user_password = Label(newWindow,text="Password")
+        self.user_password.place(x=40, y=100)
 
-        self.submit_button = Button(newWindow,text="Submit", command = self.Register_succeeded).place(x=40,y=130)
+        self.submit_button = Button(newWindow,text="Submit", command = lambda: self.Register_succeeded(newWindow))
+        self.submit_button.place(x=40, y=130)
 
-        self.user_name_input_area = Entry(newWindow,width=30).place(x=110,y=60)
+        self.user_name_input_area = Entry(newWindow,width=30)
+        self.user_name_input_area.place(x=110, y=60)
 
-        self.user_password_entry_area = Entry(newWindow, width=30).place(x=110,y=100)
+        self.user_password_entry_area = Entry(newWindow, width=30)
+        self.user_password_entry_area.place(x=110, y=100)
+
+
+    def Register_succeeded(self, newWindow):
+        # Destroy the widgets in the registration window
+        self.user_name.destroy()
+        self.user_password.destroy()
+        self.submit_button.destroy()
+        self.user_name_input_area.destroy()
+        self.user_password_entry_area.destroy()
+
+        # Create a label indicating successful registration
+        Label(newWindow, text="Registration succeeded").pack()
+
+        # Create a button to close the window
+        Button(newWindow, text="Close", command=newWindow.destroy).pack()
+
+    def Register_not_succeeded(self, newWindow):
+        # Destroy the widgets in the registration window
+        self.user_name.destroy()
+        self.user_password.destroy()
+        self.submit_button.destroy()
+        self.user_name_input_area.destroy()
+        self.user_password_entry_area.destroy()
+
+        # Create a label indicating successful registration
+        Label(newWindow, text="Couldn't register, Try Again!").pack()
+
+        # Create a button to close the window
+        Button(newWindow, text="Close", command=newWindow.destroy).pack()
 
 
 
-        # function to open a new window
-    # on a button click
     def Assign_Window(self):
         # Toplevel object which will
         # be treated as a new window
@@ -72,22 +98,55 @@ class A:
         newWindow.title("New Window")
 
         # sets the geometry of toplevel
-        newWindow.geometry("200x200")
+        newWindow.geometry("450x300")
 
-        # A Label widget to show in toplevel
-        Label(newWindow,
-              text="This is a new window").pack()
-
-
-
-    def Register_succeeded(self):
         # the label for user_name
+        self.user_name = Label(newWindow, text="Username")
+        self.user_name.place(x=40, y=60)
+
+        # the label for user_password
+        self.user_password = Label(newWindow, text="Password")
+        self.user_password.place(x=40, y=100)
+
+        self.submit_button = Button(newWindow, text="Submit", command=lambda: self.Assign_succeeded(newWindow))
+        self.submit_button.place(x=40, y=130)
+
+        self.user_name_input_area = Entry(newWindow, width=30)
+        self.user_name_input_area.place(x=110, y=60)
+
+        self.user_password_entry_area = Entry(newWindow, width=30)
+        self.user_password_entry_area.place(x=110, y=100)
+
+
+
+    def Assign_succeeded(self, newWindow):
+        # Destroy the widgets in the log in window
         self.user_name.destroy()
         self.user_password.destroy()
         self.submit_button.destroy()
         self.user_name_input_area.destroy()
         self.user_password_entry_area.destroy()
 
+        # Create a label indicating successful registration
+        Label(newWindow, text="You Logged In successfuly!").pack()
+
+        # Create a button to close the window
+        Button(newWindow, text="Close", command=newWindow.destroy).pack()
+
+
+    def Assign_not_succeeded(self, newWindow):
+        # Destroy the widgets in the log in window
+        self.user_name.destroy()
+        self.user_password.destroy()
+        self.submit_button.destroy()
+        self.user_name_input_area.destroy()
+        self.user_password_entry_area.destroy()
+
+        # Create a label indicating successful registration
+        Label(newWindow, text="Couldn't Log In, Try Again!").pack()
+
+        # Create a button to close the window
+        Button(newWindow, text="Close", command=newWindow.destroy).pack()
 
 
 def Main():
