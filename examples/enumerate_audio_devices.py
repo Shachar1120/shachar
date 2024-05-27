b2ab4f13cc4ca82befb3e7e12bea5ee5346f4463
shachar <shachar0232@gmail.com>
2024-05-27 16:00:10 +0300

    adding pictures for ring animation

--- examples/enumerate_audio_devices.py
+++ examples/enumerate_audio_devices.py
@@ -1,13 +1,14 @@
 import pyaudio
 
 def select_microphone(index):
-   # Get the device info
-   device_info = p.get_device_info_by_index(index)
-   # Check if this device is a microphone (an input device)
-   if device_info.get('maxInputChannels') > 0:
+    # Get the device info
+    device_info = p.get_device_info_by_index(index)
+    # Check if this device is a microphone (an input device)
+    if device_info.get('maxInputChannels') > 0:
       print(f"Selected Microphone: {device_info.get('name')}")
-   else:
+    else:
       print(f"No microphone at index {index}")
+    #
 
 # Select a microphone with a specific index
 
