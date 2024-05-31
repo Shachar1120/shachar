49d38518165a1699c2f6326ea9348308858bd49e
Shachar1120 <148318799+Shachar1120@users.noreply.github.com>
2024-05-31 09:12:08 +0300

    Update new_protocol.py

--- new_protocol.py
+++ new_protocol.py
@@ -168,7 +168,7 @@ class Pro:
         msg_len = len(msg_to_send)
         return str(msg_len).zfill(Pro.LENGTH_FIELD_SIZE).encode() + msg_to_send
 
-    @staticmethod
+    @staticmethod 
     def get_msg(socket_to_server):
         """
         Extract message from protocol, without the length field
