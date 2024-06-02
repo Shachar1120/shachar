091bff9d8da50fc79db81199bc853d895f4067c3
shachar1120 <shachar0232@gmail.com>
2024-06-02 14:13:19 +0300

    FRAME pickle loads failed &&& ringing panel doesnt change to in call!!!

--- example.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter.ttk import *
-root = Tk()
-
-photo = PhotoImage(file=r"C:\שחר\tkinter\ringing1.png")
-photoimage = photo.subsample(3, 3)
-btn_calling = Button(root, image=photoimage)
-btn_calling.place(x=30, y=100)
